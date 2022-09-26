328e80321d288a1d087b8630f6907fb6ad2efad2
Yurysi <92383030+Yurysi@users.noreply.github.com>
2022-09-26 19:05:06 +0300

    Task 7_3

---
+++ task_7_3.py
@@ -0,0 +1,21 @@
+from random import randint
+
+"""
+    Task 3
+
+    List comprehension exercise
+
+    Use a list comprehension to make a list containing tuples (i, j) where
+    `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
+"""
+
+num_list = []
+tpl = []
+for k in range(25):
+        i = randint(1, 10)
+        j = i ** 2
+        tpl = [i, j]
+        num_list.append(tpl)
+num_list.append(tpl)
+
+print(num_list)
